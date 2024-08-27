d8fecd8972ceb466085618edf502d7f7737ba27d
Abel Castro <127796221+AbeCasMar@users.noreply.github.com>
2024-08-26 22:28:35 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,89 @@
+var continuar = "si";
+var opcion = prompt(`¿Que operación deseas realizar?, presiona la tecla correspondiente:
+    
+    1-Sumar.
+    2-Restar.
+    3-Multiplicar.
+    4-Dividir.
+    
+    `);
+
+
+function sumar() {
+
+    let num1 = parseInt(prompt("Ingresa el primer número."));
+    let num2 = parseInt(prompt("Ingresa el segundo número."));
+    let resultado = num1 + num2;
+
+    alert(`Resultado: ${resultado}`);
+
+}
+
+function restar() {
+
+    let num1 = parseInt(prompt("Ingresa el primer número."));
+    let num2 = parseInt(prompt("Ingresa el segundo número."));
+    let resultado = num1 - num2;
+
+    alert(`Resultado: ${resultado}`);
+
+}
+
+function multiplicar() {
+
+    let num1 = parseInt(prompt("Ingresa el primer número."));
+    let num2 = parseInt(prompt("Ingresa el segundo número."));
+    let resultado = num1 * num2;
+
+    alert(`Resultado: ${resultado}`);
+
+}
+
+function dividir() {
+
+    let num1 = parseInt(prompt("Ingresa el primer número."));
+    let num2 = parseInt(prompt("Ingresa el segundo número."));
+    let resultado = num1 / num2;
+
+    alert(`Resultado: ${resultado}`);
+
+}
+
+
+while(continuar == "si"){
+
+
+    switch (opcion) {
+        case "1":
+            sumar();
+            break;
+        case "2":
+            restar();
+            break;
+        case "3":
+            multiplicar();
+            break;
+        case "4":
+            dividir();
+            break;
+        default:
+            alert("No se ingreso una opción valida");
+            break;
+    }
+
+    continuar = prompt("Deseas realizar otra operacion? escribe si o no para salir")
+    if(continuar == "si"){
+
+        opcion = prompt(`¿Que operación deseas realizar?, presiona la tecla correspondiente:
+    
+            1-Sumar.
+            2-Restar.
+            3-Multiplicar.
+            4-Dividir.
+            
+            `);
+        
+
+    }
+    
+}
